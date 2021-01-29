d1d482e0d3154c2e7f025649b7279d7e147538d5
Joshua Sacuedo <joshuansu0897@gmail.com>
2021-01-28 22:46:41 -0500

    change to pg

--- src/services/db.js
+++ src/services/db.js
@@ -3,10 +3,10 @@ const Sequelize = require('sequelize')
 const util = require('../utils/errorhandler')
 
 const name = process.env.NAME_DB || 'ExampleDB'
-const user = process.env.USER_DB || 'root'
+const user = process.env.USER_DB || 'postgres'
 const pass = process.env.PASS_DB || 'example'
 const host = process.env.HOST_DB || 'localhost'
-const type = process.env.TYPE_DB || 'mysql'
+const type = process.env.TYPE_DB || 'postgres'
 const port = process.env.PORT_DB || 3306
 
 const sequelize = new Sequelize(name, user, pass, {
